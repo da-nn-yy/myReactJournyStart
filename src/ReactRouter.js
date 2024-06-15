4ab182dcd5c7a71ebb8c32a2ede6bff8f30aee7f
da-nn-yy <db7394928@gmail.com>
2024-06-16 00:24:45 +0200

    modified: ReactRouter.js

--- src/ReactRouter.js
+++ src/ReactRouter.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes,Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes,Route ,Link } from "react-router-dom";
 import ExcuseFetchExe from "./ExcuseFectchExe"
 import Fetching from "./Fetching"
 import Fetchingg from "./Fetchingg"
@@ -6,11 +6,18 @@ import Fetchingg from "./Fetchingg"
 const ReactRouter = () => {
   return (
     <Router>
+      <div style={{display:"flex", justifyContent:"space-around", marginBottom:"3rem"}}>
+        <Link to="/">Home</Link>
+        <Link to="/Excuse">Excuse</Link>
+        <Link to="/Fetching1">Fetching</Link>
+        <Link to="/Fetching2">Fetchingg</Link>
+      </div>
       <Routes>
         <Route path="/" element={<h1>HI</h1>}/>
         <Route path="/Excuse" element={<ExcuseFetchExe/>}/>
         <Route path="/Fetching1" element={<Fetching/>}/>
         <Route path="/Fetching2" element={<Fetchingg/>}/>
+        <Route path="*" element={<h1>Page not found</h1>}/>
       </Routes>
     </Router>
 
