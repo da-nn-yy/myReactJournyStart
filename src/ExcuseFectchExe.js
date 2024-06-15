2442e2c4e26c87a3bdacbcc3eea9d05f984ddbf2
da-nn-yy <db7394928@gmail.com>
2024-06-15 23:08:48 +0200

    added: three button functionality

--- src/ExcuseFectchExe.js
+++ src/ExcuseFectchExe.js
@@ -1,27 +1,25 @@
 import Axios from "axios";
 import { useState } from "react";
 const ExcuseFetchExe = () => {
-  const [excuse,setExcuse] = useState("")
-
-  const fetchExcuse = () => {
-  Axios.get(`https://excuser-three.vercel.app/v1/excuse/${excuse}/`).then((res => {
-    console.log(res.data)
-  }));
+  const [genetratedExcuse,setGeneratedExcuse] = useState("")
+  const fetchExcuse = (excuse) => {
+    Axios.get(`https://excuser-three.vercel.app/v1/excuse/${excuse}/`).then((res => {
+    setGeneratedExcuse(res.data[0].excuse)}))
+  }
   
-
   return (
     <div>
      
       <h1>Get Excuse for:</h1>
       <div style={{margin:"50px", display:"flex",justifyContent:"space-between" }}>
-        <button onClick={fetchExcuse}>Family</button>
-        <button>Office</button>
-        <button>Gaming</button>
+        <button onClick={() => fetchExcuse("family")}>Family</button>
+        <button onClick={() => fetchExcuse("Office")}>Office</button>
+        <button onClick={() => fetchExcuse("gaming")}>Gaming</button>
       </div>
 
-      <p></p>
+      <p>{genetratedExcuse}</p>
     </div> 
     )
 }
-}
+
 export default ExcuseFetchExe;
