0ef8c1ca9f70a32ccc926bc1b8b7946219254ccc
da-nn-yy <db7394928@gmail.com>
2024-06-16 14:57:59 +0200

    modified: Profile router bu useParams

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,11 +1,13 @@
-// import { useNavigate } from "react-router-dom";  //Help us for Route using useNavigate function.
+// import { useNavigate ,useParams } from "react-router-dom";  //Help us for Route using useNavigate function. .....AND .... using useParams for different profiles..example: .com/Profile/user
 import React from 'react'
 
 function Profile() {
   // let navigate = useNavigate()   /// function for navigate
+  // let {username} = useParams()   /// function for useParams
   return (
     <div>
-      <h1>Profile</h1>
+
+      <h1>Profile {/*{username}*/}</h1>
   {/* <button onClick={() => navigate('/home')}>Go to Home page</button> */} {/*This is useNavigate use */}
     </div>
   )
