01c6e53f4985a199428fe9a7ac7993b749baa920
da-nn-yy <db7394928@gmail.com>
2024-07-04 22:35:00 +0200

    modified: by adding useQuery and QueryClinetProvider

--- src/DanRctQuery/Rctquery.js
+++ src/DanRctQuery/Rctquery.js
@@ -4,20 +4,25 @@ import { Navbar } from './Navbar'
 import {Home} from './pages/Home';
 import {Profile} from './pages/Profile';
 import {Contact} from './pages/Contact';
+import { QueryClient,QueryClientProvider } from '@tanstack/react-query';
 
 
 const RctQu = () => {
+
+  const client = new QueryClient();
   return (
     <div className='App'>
-      <Router>
-        <Navbar/>
-        <Routes>
-          <Route path='/' element={<Home/>}/>
-          <Route path='/profile' element={<Profile/>}/>
-          <Route path='/contact' element={<Contact/>}/>
-          <Route path='*' element={<h1>Page not Found</h1>}/>
-        </Routes>
-      </Router>
+      <QueryClientProvider client={client}>
+        <Router>
+          <Navbar/>
+          <Routes>
+            <Route path='/' element={<Home/>}/>
+            <Route path='/profile' element={<Profile/>}/>
+            <Route path='/contact' element={<Contact/>}/>
+            <Route path='*' element={<h1>Page not Found</h1>}/>
+          </Routes>
+        </Router>
+      </QueryClientProvider>
 
     </div>
   )
--- src/DanRctQuery/pages/Home.js
+++ src/DanRctQuery/pages/Home.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import { useQuery } from "@tanstack/react-query"
+import Axios from "axios";
+
 
 export const Home = () => {
+  const { data, isLoading ,isError ,refetch} = useQuery({
+    queryKey:["cat"], 
+    queryFn:() => Axios.get("https://catfact.ninja/factss").then((res) => res.data)
+  });
+
+  if (isLoading) {return <div>Loading...</div>;}
+  
+  if(isError) {
+    return <h1>Sorry,there was an error</h1>;
+  }
+
+  
   return (
     <div>
-      <h1>This is Home Page</h1>
+      <h1>This is Home Page <p>{data?.fact}</p></h1>
+      <button onClick={refetch}>Update Data</button>
     </div>
   )
 }
