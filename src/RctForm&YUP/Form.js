b8dbab8d46b7946cb7b1fded43c5f7dce15ef303
da-nn-yy <db7394928@gmail.com>
2024-07-07 23:07:21 +0200

    installed resolvers and edit my code

--- src/RctForm&YUP/Form.js
+++ src/RctForm&YUP/Form.js
@@ -1,10 +1,21 @@
 import { useForm } from "react-hook-form"
 import * as yup from 'yup';
+import { yupResolver } from "@hookform/resolvers/yup";
 
 export const Form = () => {
 
-  const {register, handleSubmit} = useForm();
-
+  
+  const schema = yup.object().shape({
+    fullName: yup.string().required(),
+    email: yup.string().email().required(),
+    age: yup.number().positive().integer().min(18).required(),
+    password: yup.string().min(4).required(),
+    confirmPass:yup.string().oneOf([yup.ref("password"),null]).required(),
+  })
+  const {register, handleSubmit} = useForm({
+    resolver: yupResolver(schema),
+  });
+  
   const onSubmit = (data) => {
     console.log(data)
   }
@@ -14,7 +25,7 @@ export const Form = () => {
       <input type="text" placeholder="Full Name..." {...register("fullName")}/>
       <input type="email" placeholder="Email..." {...register("email")}/>
       <input type="number" placeholder="Age..." {...register("age")}/>
-      <input type="password" placeholder="Password..." {...register("Password")}/>
+      <input type="password" placeholder="Password..." {...register("password")}/>
       <input type="password" placeholder="Confirm Passwod..." {...register("confirmPass")}/>
       <input type="submit"/>
     </form>
