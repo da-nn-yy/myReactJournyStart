170f628ac7316002dae5bdc36720e7330271bc74
da-nn-yy <db7394928@gmail.com>
2024-07-08 22:41:15 +0200

    modifeid : catFact.js

--- src/DANCustomHook/Catfact.js
+++ src/DANCustomHook/Catfact.js
@@ -3,13 +3,24 @@ import { QueryClient,QueryClientProvider } from '@tanstack/react-query';
 
 export const Catfact = () => {
 
-  const {data:catFact,reftch,isLoding} = useCatFact()
+  // const {data:catFact,reftchData,isCatLoding} = useCatFact() 
+  const {data,reftchData,isCatLoding} = useCatFact() 
   
-  const client = new QueryClient()
+   const client = new QueryClient({defaultOptions:
+    {
+      queries:{
+        // refetchOnWindowFocus:true, //if its true teh page will fetch another data while i go from one tab another tab
+        refetchOnWindowFocus:false, //if its false the page doesnt fetch another data 
+      }
+    }
+  });
+  if (isCatLoding) return <h1>loading ... </h1>
 
   return (
     <QueryClientProvider client={client}>
-      <h1>{catFact?.fact}</h1>
+      {/* <h1>{catFact?.fact}</h1> */}
+      <h1>{data?.fact}</h1>
+      <button onClick={reftchData}>Refetch</button>
     </QueryClientProvider>
     
   )
