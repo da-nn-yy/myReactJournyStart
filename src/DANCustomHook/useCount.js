1f828e785e75ac3cd1e9432605b468965105d520
da-nn-yy <db7394928@gmail.com>
2024-07-09 00:05:27 +0200

    modified: useCount custom hook

--- src/DANCustomHook/useCount.js
+++ src/DANCustomHook/useCount.js
@@ -1,20 +1,25 @@
 import { useState } from "react";
 
 
-
+// export const useCount = (instialVal = 0) => {
 export const useCount = () => {
   
-  const[count,setCount] = useState(0);
+  const[count,setCount] = useState();
+  // const[count,setCount] = useState(intialVal);
+
 
   const incr = () => {
-    setCount(count + 1)
+    setCount(count + 1);
+    // setCount((prev => prev + 1))
   }
   const decr = () => {
-    setCount(count - 1)
+    setCount(count - 1);
+    // setCount((prev => prev - 1))
+
   }
   const restart = () => {
-    setCount(0)
+    setCount(0);
   }
 
   return {count,incr,decr,restart}
-}
+};
