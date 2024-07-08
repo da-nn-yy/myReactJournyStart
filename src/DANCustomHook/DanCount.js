773665e2065941c440a27592ea8195a2b243d011
da-nn-yy <db7394928@gmail.com>
2024-07-08 23:48:27 +0200

    created new Count.js and useCount hook

---
+++ src/DANCustomHook/DanCount.js
@@ -0,0 +1,13 @@
+import { useState } from "react"
+
+const DanCount = () => {
+  
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default DanCount
---
+++ src/DANCustomHook/useCount.js
@@ -0,0 +1,9 @@
+import { useState } from "react";
+
+
+
+export const useCount = () => {
+  use 
+
+  return ()
+}
