980aedd4033a6825b62db42fc661f3d0f9b4f23e
da-nn-yy <db7394928@gmail.com>
2024-07-08 22:58:09 +0200

    modified:  but with error

--- src/DANCustomHook/Catfact.js
+++ src/DANCustomHook/Catfact.js
@@ -4,24 +4,22 @@ import { QueryClient,QueryClientProvider } from '@tanstack/react-query';
 export const Catfact = () => {
 
   // const {data:catFact,reftchData,isCatLoding} = useCatFact() 
-  const {data,reftchData,isCatLoding} = useCatFact() 
+  const {data,refetchData,isCatLoding} = useCatFact() 
   
-   const client = new QueryClient({defaultOptions:
-    {
-      queries:{
-        // refetchOnWindowFocus:true, //if its true teh page will fetch another data while i go from one tab another tab
-        refetchOnWindowFocus:false, //if its false the page doesnt fetch another data 
-      }
-    }
-  });
+   const client = new QueryClient();
   if (isCatLoding) return <h1>loading ... </h1>
 
   return (
-    <QueryClientProvider client={client}>
-      {/* <h1>{catFact?.fact}</h1> */}
-      <h1>{data?.fact}</h1>
-      <button onClick={reftchData}>Refetch</button>
-    </QueryClientProvider>
-    
+
+    <div>
+      <QueryClientProvider client={client}>
+        <div>
+          {/* <h1>{catFact?.fact}</h1> */}
+          <h1>{data?.fact}</h1>
+          <button onClick={refetchData}>Refetch</button>
+        </div>
+      </QueryClientProvider>
+      
+    </div>
   )
-}
+};
