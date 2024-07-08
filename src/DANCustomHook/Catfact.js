e852242ade67490199adb76391f8ca08cb6e0c60
da-nn-yy <db7394928@gmail.com>
2024-07-08 22:25:46 +0200

    modified: Catfact.js by importing my useCatFact hook

--- src/DANCustomHook/Catfact.js
+++ src/DANCustomHook/Catfact.js
@@ -1,12 +1,15 @@
 import { useCatFact } from './useCatFact';
 import { QueryClient,QueryClientProvider } from '@tanstack/react-query';
+
 export const Catfact = () => {
+
+  const {data:catFact,reftch,isLoding} = useCatFact()
   
   const client = new QueryClient()
 
   return (
     <QueryClientProvider client={client}>
-      <h1>{catData?.fact}</h1>
+      <h1>{catFact?.fact}</h1>
     </QueryClientProvider>
     
   )
