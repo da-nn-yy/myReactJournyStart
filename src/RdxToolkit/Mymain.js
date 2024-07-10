417fc8dbeeaef49a161a9982c9f2833c21d8d9ef
da-nn-yy <db7394928@gmail.com>
2024-07-10 16:36:35 +0200

    modified : my main

--- src/RdxToolkit/Mymain.js
+++ src/RdxToolkit/Mymain.js
@@ -2,10 +2,13 @@ import {BrowserRouter as Router ,Routes,Route} from "react-router-dom"
 import {Home} from './Pages/Home';
 import {Login} from './Pages/Login';
 import {Contact} from './Pages/Contact';
+import { Provider } from "react-redux";
+import store from "./store";
 
 const Mymain = () => {
   return (
     <div>
+      <Provider store={store}>
       <Router>
         <Routes>
           <Route path="/" element={<Home/>}/>
@@ -13,6 +16,7 @@ const Mymain = () => {
           <Route path="/contact" element={<Contact/>}/>
         </Routes>
       </Router>
+      </Provider>
     </div>
   )
 }
